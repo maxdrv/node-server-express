4d5e247f0da7b9ba2396d6497152f4868cf96194
maxdrv <maxdrv.stud@gmail.com>
2022-10-18 23:55:53 +0500

    auth controller refactor

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,21 @@
 const usersDB = {
     users: require('../model/users.json'),
-    setUsers: function (data) {
-        this.users = data;
-    }
+    setUsers: function (data) { this.users = data }
 }
-
 const bcrypt = require('bcrypt');
 
 const handleLogin = async (req, res) => {
-    const {user, pwd} = req.body;
-    if (!user || !pwd) return res.status(400).json({'message': 'Username and password are required'})
-
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
-    if (!foundUser) return res.status(401);  // Unauthorized
-
+    if (!foundUser) return res.sendStatus(401); //Unauthorized
+    // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-        // here we will create jwt in the future
-        res.json({'success': `User ${user} is logged in!`})
+        // create JWTs
+        res.json({ 'success': `User ${user} is logged in!` });
     } else {
-        res.sendStatus(401);  // Unauthorized
+        res.sendStatus(401);
     }
 }
 
