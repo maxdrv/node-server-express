ea82d2c8c2ee6aa4ee71cf94c8123eccbaadcf66
maxdrv <maxdrv.stud@gmail.com>
2022-10-20 10:40:36 +0500

    added comment

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,5 +1,7 @@
 const allowedOrigins = require('../config/allowedOrigins');
 
+
+// if request came from domain that we allow, set allow credentials to true, so browser can use response
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
