a818c1af21b36505995cbfb479b13765f8526c71
maxdrv <maxdrv.stud@gmail.com>
2022-10-22 11:58:25 +0500

    added role verification to routes

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const {getAllEmployees, updateEmployee, createNewEmployee, deleteEmployee, getEmployee} = require("../../controllers/employeesController");
+const ROLES_LiST = require('../../config/role_list');
+const verifyRoles = require('../../middleware/verifyRoles')
 
 router.route('/')
     .get(getAllEmployees)
-    .post(createNewEmployee)
-    .put(updateEmployee)
-    .delete(deleteEmployee);
+    .post(
+        verifyRoles(ROLES_LiST.Admin, ROLES_LiST.Editor),
+        createNewEmployee
+    )
+    .put(
+        verifyRoles(ROLES_LiST.Admin, ROLES_LiST.Editor),
+        updateEmployee
+    )
+    .delete(
+        verifyRoles(ROLES_LiST.Admin),
+        deleteEmployee
+    );
 
 router.route('/:id')
     .get(getEmployee);
